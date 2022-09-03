5fca71f3c805d21d81730ac0db8182c0b8d4a73d
rherik <84604794+rherik@users.noreply.github.com>
2022-09-03 19:43:19 -0300

    Adiciona campo de busca

--- lista/urls.py
+++ lista/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('busca/', views.busca, name='busca'),
     path('<int:filme_id>', views.ver_filme, name='ver_filme'),
+
 ]
--- lista/views.py
+++ lista/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import ListaDosFilmes
 from django.core.paginator import Paginator
-
-# Create your views here.
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
-    filmes = ListaDosFilmes.objects.all()
+    filmes = ListaDosFilmes.objects.order_by('-id').filter(
+        mostrar=True
+    )
     paginator = Paginator(filmes, 10)
 
     page = request.GET.get('p')
@@ -19,6 +22,32 @@ def index(request):
 
 def ver_filme(request, filme_id):
     filme = get_object_or_404(ListaDosFilmes, id=filme_id)
+
+    if not filme.mostrar:
+        raise Http404
     return render(request, 'lista/ver_filme.html', {
         'filme': filme
     })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        raise Http404()
+    campos = Concat('nome', Value(' '), 'direcao')
+
+    filmes = ListaDosFilmes.objects.annotate(
+        nome_e_direcao=campos
+    ).filter(
+        Q(nome_e_direcao__icontains=termo) | Q(genero__icontains=termo)
+    )
+    paginator = Paginator(filmes, 10)
+
+    page = request.GET.get('p')
+    filmes = paginator.get_page(page)
+
+    return render(request, 'lista/busca.html', {
+        'filmes': filmes
+    })
+
